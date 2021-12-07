6dbde0d0a49b265e89bed6ef7afe20d31ca2290b
Parallexia <parallexia@outlook.com>
2021-12-07 17:59:07 +0800

    实现了发送session会话

---
+++ server/project/passportv2/session.go
@@ -0,0 +1,53 @@
+package passportv2
+
+import (
+	"fmt"
+	"log"
+
+	tsgutils "github.com/typa01/go-utils"
+
+	"github.com/garyburd/redigo/redis"
+)
+
+func NewUserSession(id string) (string, error) {
+
+	c := DialRedis()
+	defer c.Close()
+	session := tsgutils.GUID()
+	expireTime := 24 * 60 * 60
+
+	_, err := c.Do("SET", "mykey", id, "EX", expireTime)
+	if err != nil {
+		log.Print("redis set failed:", err)
+	}
+
+	return session, nil
+}
+
+func VerifyUserSession(session string) (bool, error) {
+
+	c := DialRedis()
+	defer c.Close()
+	exist, err := redis.Bool(c.Do("EXISTS", "mykey1"))
+	if err != nil {
+		log.Print("redis query failed:", err)
+		return false, err
+	}
+
+	if exist {
+		return true, nil
+	}
+	return false, nil
+}
+
+func ResetSessionTime(session string) error {
+
+	c := DialRedis()
+	defer c.Close()
+	n, err := c.Do("EXPIRE", session, 24*60*60)
+	if n == int64(1) {
+		fmt.Print("success")
+		return nil
+	}
+	return err
+}
